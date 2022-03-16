1792ced6b085c733ef708f745052b76197f41027
Denis_Dragomir <Denis.Dragomir@Dell.com>
2022-03-16 20:20:15 +0200

    Initialized SimpliLearn repository

---
+++ Live classes/1. Agile XP/notes.js
@@ -0,0 +1,62 @@
+// Waterfall method -> series of concrete steps
+
+// Development -> QA -> validation against the design (robustness, no bugs)
+// If the specifications change, we can not change our current implementation
+
+/* SCRUM PRACTICES */
+
+// 1. Product backlog
+// Ordered list of tasks to be done in the product
+// Living document (the priority of its elements may change over time)
+// Tasks are prioritized and ordered accordingly
+// The product owner is responsible for it
+
+// 2. Story (user stories)
+// Self contained bit of functionality
+// Describes a task which will deliver a particular value to a customer
+// It has: name, description, estimated value, estimated effort, risks, dependencies, acceptance tests
+
+// 3. Sprint backlog
+// A timebox (1 week, 2 weeks, 4 weeks) specific to scrum
+// A list o items (tasks) identified by the scrum team, which must be implemented within the estimated time
+// Maintained by the development team
+
+// Tasks or stories are moved from the product backlog to the sprint backlog
+
+// 4. Timeboxing
+// The process of allocationg an amount of time to a specifing item in the product/sprint backlog
+// Prioritizes tasks
+// Tracks progress
+// Improves predictability
+// Improves time management
+// Establishes WIP limit
+
+// 5. DoD -- definition of done
+// Things that must be verified before marking a story/task as "complete"
+// It can apply to: a user story, a sprint, a release, a project
+
+/* SCRUM PRACTICES */
+
+/* PLANNING */
+
+// Layers
+
+// 1. Strategy
+// Definition and execution if strategic goals
+
+// 2. Portofolio
+// Product offerings
+
+// 3. Product
+// Product -> roadmap relation
+
+// 4. Daily
+// Plan of action
+
+// 5. Sprint
+// User stories that can be completed within a sprint
+
+// 6. Release
+// Product backlog items gouped into smaller releases
+
+/* PLANNING */
---
+++ Live classes/general.js
@@ -0,0 +1,10 @@
+// General structure:
+// 55 min -> 20 min break -> 40 min -> 5 min break -> 55 min -> 5 min break -> 1 hour
+
+// OSL -> Off site learning -> learn (e-learning) on our own
+// Mentoring -> going through the material (e-learning) and ask questions (similar to live classes)
+// Lab sessions -> practice
+
+// After the last session, we'll have 1 month to complete the final project (Capstone Project)
+
+// Repository -> https://github.com/cfjames/simplilearn_dotnetfsd2
