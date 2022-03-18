0aaa226f69a1a53fc5314e4b39ea1ebec3d96b87
Denis_Dragomir <Denis.Dragomir@Dell.com>
2022-03-18 12:25:44 +0200

    Program

--- C# 8 Programming/Hello World/Program.cs
+++ C# 8 Programming/Hello World/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+static void DeclareEmployee()
+{
+    Customer customer;
+    customer.Age = 35;
+    customer.Name = "John";
+    customer.IsEmployee = true;
+    Console.WriteLine($"{customer.Name} is a {customer.Age} years old employee");
+}
+
+
 Console.WriteLine("Hello, World!");
+
+string? name = Console.ReadLine();
+Console.WriteLine($"Hi, {name}. How are you doing?");
+
+DeclareEmployee();
+
+public struct Customer
+{
+    public int Age;
+    public string Name;
+    public bool IsEmployee;
+}
