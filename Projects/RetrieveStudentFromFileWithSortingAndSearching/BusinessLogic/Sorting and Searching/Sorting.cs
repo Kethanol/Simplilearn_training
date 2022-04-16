cffb684d1982461ec7a55f153b275e1a04966ef9
Denis_Dragomir <Denis.Dragomir@Dell.com>
2022-04-16 17:17:35 +0300

    Added BL Tier

---
+++ Projects/RetrieveStudentFromFileWithSortingAndSearching/BusinessLogic/Sorting and Searching/Sorting.cs
@@ -0,0 +1,16 @@
+﻿namespace BusinessLogic.Sorting_and_Searching
+{
+    public class Sorting<T> where T : class
+    {
+        public List<T> List { get; set; }
+
+        public Sorting(List<T> list)
+        {
+            List = list;
+        }
+
+        public void Bubble() { 
+
+        }
+    }
+}
