60cf4c4be624444aa8424ec49711b2246c63c002
Denis_Dragomir <Denis.Dragomir@Dell.com>
2022-04-16 17:02:25 +0300

    New project

---
+++ Projects/RetrieveStudentFromFileWithSortingAndSearching/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace RetrieveStudentFromFile
+{
+    internal class Program
+    {
+        static void ReadAndProcessText(string path) {
+            var students = File.ReadAllLines(path);
+
+            foreach (var student in students) {
+                var studentData = student.Split(',');
+                Console.WriteLine($"The student's name is {studentData[0]}, they are {studentData[1]} years old and they are attending {studentData[2]}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var path = $"{Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName}{Constants.File.PATH}";
+
+            if (!File.Exists(path)) {
+                throw new FileNotFoundException(Constants.File.ERROR);
+            }
+
+            ReadAndProcessText(path);
+        }
+    }
+}
