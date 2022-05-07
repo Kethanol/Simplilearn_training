597cc45ff30cda63ff7b6009e33b28ba6007d171
Denis_Dragomir <Denis.Dragomir@Dell.com>
2022-05-07 23:17:04 +0300

    Save

--- Projects/DataManager - Nuget/Executor/Properties/AssemblyInfo.cs
+++ Projects/DataManager - Nuget/Executor/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Executor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DataManager.Library")]
+[assembly: AssemblyDescription("FSD Learning Package")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dell, Inc.")]
 [assembly: AssemblyProduct("Executor")]
